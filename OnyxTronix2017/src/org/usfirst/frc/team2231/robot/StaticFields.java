65f4978c79f3975f27757cfbd71ec99f74d3e7e6
NadavZiri <nadavziri123@gmail.com>
2017-01-30 22:46:48 +0200

    Removed StaticFields.
    Removed StaticFields class.

--- OnyxTronix2017/src/org/usfirst/frc/team2231/robot/StaticFields.java
+++
@@ -1,9 +0,0 @@
-package org.usfirst.frc.team2231.robot;
-
-public interface StaticFields {
-
-	double SHOOTER_SPEED = 1;
-	double P_PID_SHOOT = 10;
-	double D_PID_SHOOT = 0.4;
-	double SHOOTER_ABSOLUTE_TOLERANCE = 5;
-}
